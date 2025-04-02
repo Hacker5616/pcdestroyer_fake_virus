ea36e5904e28bb8b72932100ddc8bfbd4d288779
Hacker5616 <101528136+Hacker5616@users.noreply.github.com>
2025-04-03 02:41:36 +0300

    Update pcdestroyer.py

--- pcdestroyer.py
+++ pcdestroyer.py
@@ -17,23 +17,6 @@ webbrowser.open ("https://duckduckgo.com/?t=ffab&q=sans+x+paapyrus+ship&iax=imag
 time.sleep(4)
 webbrowser.open ("https://www.youtube.com/watch?v=QqHqP_yVCRY")
 
-time.sleep(23)
-webbrowser.open ("https://www.youtube.com/watch?v=KcPFKKu49sc")
-time.sleep(10)
-webbrowser.open ("https://www.youtube.com/watch?v=VJT1NA38tJ8")
-
-time.sleep(12)
-webbrowser.open ("https://www.youtube.com/watch?v=ksKAgbFDyUU")
-time.sleep(23)
-webbrowser.open ("https://www.youtube.com/watch?v=2-H7DVE8_PM")
-time.sleep(18)
-webbrowser.open ("https://www.youtube.com/watch?v=b4XpMTUlorc")
-time.sleep(20)
-webbrowser.open ("https://www.youtube.com/watch?v=XsAI0RqoiTA")
-time.sleep(4)
-webbrowser.open ("https://www.youtube.com/watch?v=bNy341mHHjs")
-time.sleep (5)
-
 root = tk.Tk()
 root.withdraw()  # Hide the main window
 
@@ -58,5 +41,3 @@ if response:
     # Run the tkinter event loop for the image window
     image_window.mainloop()
 
-time.sleep(1)
-os.system("shutdown /s /t 30")
